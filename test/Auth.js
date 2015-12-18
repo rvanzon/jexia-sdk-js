116ed662815b093120187f74e8150c0e19ad1806
d3nnn1z <d3nnn1z@gmail.com>
2015-12-18 18:26:19 +0100

    test coverage

--- test/Auth.js
+++ test/Auth.js
@@ -25,5 +25,28 @@ describe('Class: Auth', () => {
     it('should not be constructed without params', () => {
         expect(() => {new Auth()}).to.throw(Error);
     });
-    
+
+    it('should return an error if credentials are not valid', (done) => {
+        nock('http://foo.app.jexia.com')
+            .post('/', { key: 'bar', secret: 'baz' })
+            .reply(403, {
+                message: "Invalid credentials"
+            });
+
+        let auth = new Auth({
+            url: 'http://' + 'foo' + '.app.jexia.com/',
+            key: 'bar',
+            secret: 'baz',
+            client: {
+                bus: {}
+            }
+        });
+
+        return auth.init().then( (app) => {
+        }, (error) => {
+            assert.equal(error, 'Invalid credentials');
+            done();
+        });
+    });
+
 });
